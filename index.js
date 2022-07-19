e1321695b9faceec86e7a8906038b28276c7ac2f
swandzel <s.wandzel@o2.pl>
2022-07-19 15:16:18 +0200

    package fix

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB connected");
   })
